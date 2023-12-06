659f8302f2b8e91bccc6ce240729d3a85fe7a6c3
undine_20 <lena_sheina@bk.ru>
2023-12-06 14:31:20 +0300

    change cross filter (test)

--- src/main/java/ru/vsu/cs/sheina/authservice/configuration/CorsFilter.java
+++ src/main/java/ru/vsu/cs/sheina/authservice/configuration/CorsFilter.java
@@ -14,7 +14,7 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+        response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "*");
         chain.doFilter(req, res);
