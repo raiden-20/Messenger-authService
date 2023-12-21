528ee3ff400505673be80a63d0c6fd6ea05b5b46
undine_20 <lena_sheina@bk.ru>
2023-12-21 17:33:11 +0300

    fix

--- src/main/java/ru/vsu/cs/sheina/authservice/dto/field/IdDTO.java
+++ src/main/java/ru/vsu/cs/sheina/authservice/dto/field/IdDTO.java
@@ -2,11 +2,13 @@ package ru.vsu.cs.sheina.authservice.dto.field;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class IdDTO {
     UUID id;
 }
